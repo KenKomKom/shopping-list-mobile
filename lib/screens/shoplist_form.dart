4fc14710b77d653311021a23a97de4d8105bfe5d
KenKomKom <119410845+KenKomKom@users.noreply.github.com>
2023-11-08 14:36:20 +0700

    TUTORIAL7

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -142,8 +142,9 @@ class _ShopFormPageState extends State<ShopFormPage> {
                               ],
                             );
                           },
-                        );}
-               _formKey.currentState!.reset();
+                        );
+                          _formKey.currentState!.reset();
+                        }
                       },
                       child: const Text(
                         "Save",
