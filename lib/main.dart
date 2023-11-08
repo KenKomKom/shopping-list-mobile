c328cb583a5bf6d66d01d8440a9423471741aada
KenKomKom <119410845+KenKomKom@users.noreply.github.com>
2023-11-08 14:10:04 +0700

    TUTORIAL7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/menu.dart';
+import 'package:shopping_list/shoplist_form.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/widgets/shop_card.dart';
 
-class ShopItem {
-  final String name;
-  final IconData icon;
-
-  ShopItem(this.name, this.icon);
-}
 
 class MyHomePage extends StatelessWidget {
     MyHomePage({Key? key}) : super(key: key);
@@ -17,56 +13,57 @@ class MyHomePage extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Shopping List',
-          style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color:Colors.white,
-                  ),
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Shopping List',
+            style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color:Colors.white,
+                    ),
+          ),
+          backgroundColor:Colors.indigo,
         ),
-        backgroundColor:Colors.indigo,
-      ),
-      body: SingleChildScrollView(
-        // Widget wrapper yang dapat discroll
-        child: Padding(
-          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
-          child: Column(
-            // Widget untuk menampilkan children secara vertikal
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
-                child: Text(
-                  'PBP Shop', // Text yang menandakan toko
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+        drawer: const LeftDrawer(),
+        body: SingleChildScrollView(
+          // Widget wrapper yang dapat discroll
+          child: Padding(
+            padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+            child: Column(
+              // Widget untuk menampilkan children secara vertikal
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                  child: Text(
+                    'PBP Shop', // Text yang menandakan toko
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              // Grid layout
-              GridView.count(
-                // Container pada card kita.
-                primary: true,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 3,
-                shrinkWrap: true,
-                children: items.map((ShopItem item) {
-                  // Iterasi untuk setiap item
-                  return ShopCard(item);
-                }).toList(),
-              ),
-            ],
+                // Grid layout
+                GridView.count(
+                  // Container pada card kita.
+                  primary: true,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 3,
+                  shrinkWrap: true,
+                  children: items.map((ShopItem item) {
+                    // Iterasi untuk setiap item
+                    return ShopCard(item);
+                  }).toList(),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
     }
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -84,50 +81,6 @@ class MyHomePage extends StatelessWidget {
   // State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class ShopCard extends StatelessWidget {
-  final ShopItem item;
-
-  const ShopCard(this.item, {super.key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.indigo,
-      child: InkWell(
-        // Area responsive terhadap sentuhan
-        onTap: () {
-          // Memunculkan SnackBar ketika diklik
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-          // Container untuk menyimpan Icon dan Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 /*
 class _MyHomePageState extends State<MyHomePage> {
